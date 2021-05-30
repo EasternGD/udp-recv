daac8b23ae9e4b0be1ac4564ee1d154c67b66f51
Abby, Chen <encoref9241@gmail.com>
2021-05-30 19:24:18 +0800

    [FIX] the protocal error

--- src/udp_recv.cc
+++ src/udp_recv.cc
@@ -6,7 +6,7 @@ int main(int argc, char* argv[])
     uint8_t buffer[PACKETSIZE];
     int frame_len;
 
-    TRY((sd = socket(PF_INET, SOCK_RAW, IPPROTO_UDP)));
+    TRY((sd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))));
 
     while (1) {
         TRY(frame_len = recvfrom(sd, buffer, PACKETSIZE, 0, NULL, NULL));
@@ -14,9 +14,15 @@ int main(int argc, char* argv[])
         struct ip* ip_header = (struct ip*)(buffer + ETH_HDRLEN);
         struct udphdr* udp_header = (struct udphdr*)(buffer + ETH_HDRLEN + IP4_HDRLEN);
         uint8_t *payload = (buffer + ETH_HDRLEN + IP4_HDRLEN +  UDP_HDRLEN);
-        int sd;
 
-        Hexdump(buffer, frame_len);
+        if (ip_header->ip_p != IPPROTO_UDP)
+            continue;
+
+        PrintEthernetHeader(ether_header);
+        PrintIpHeader(ip_header);
+        PrintUdpHeader(udp_header);
+
+        Hexdump(payload, frame_len - (ETH_HDRLEN + IP4_HDRLEN + UDP_HDRLEN));
     }
 
 }
