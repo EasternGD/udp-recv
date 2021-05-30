e09f99074f13462b2b7fe5b0d237f3d2294b2060
Abby, Chen <encoref9241@gmail.com>
2021-05-30 18:02:02 +0800

    [INIT] first commit

---
+++ src/udp_recv.cc
@@ -0,0 +1,25 @@
+#include "utils.h"
+
+int main(int argc, char* argv[])
+{
+    int sd;
+    uint8_t buffer[PACKETSIZE];
+    int frame_len;
+
+    TRY((sd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP)));
+
+    while (1) {
+        TRY(frame_len = recvfrom(sd, buffer, PACKETSIZE, 0, NULL, NULL));
+        struct ether_header *ether_header = (struct ether_header*)buffer;
+        struct ip* ip_header = (struct ip*)(buffer + ETH_HDRLEN);
+        struct udphdr* udp_header = (struct udphdr*)(buffer + ETH_HDRLEN + IP4_HDRLEN);
+        uint8_t *payload = (buffer + ETH_HDRLEN + IP4_HDRLEN +  UDP_HDRLEN);
+        int sd;
+
+        if (ip_header->ip_p != IPPROTO_UDP)
+            continue;
+
+        Hexdump(buffer, frame_len);
+    }
+
+}
